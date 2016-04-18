68d7a6d591bdf08c3ae3c876e41f517c8c5afc18
Tim van der Weijde <Tim van der Weijde>
2016-04-18 21:00:22 +0200

    final version

--- js/main.js
+++ js/main.js
@@ -4,13 +4,43 @@ var apiKey = "http://api.icndb.com/jokes/random?limitTo=nerdy";
 var quoteSign = "<div class=\"quote-sign\"></div>";
 
 function parseQuoteData(data) {
-    $("#quote").empty();
+    $("#quote").animateCss('zoomIn');
     
+    $("#quote").empty();
     $("#quote").append(data.value.joke);
+     
+    changeTweetLink(data.value.joke);
 }
 
+function changeTweetLink(quote) {
+    console.log('changeTweetLink:' + quote);
+  if (quote && quote.length > 0) {
+    var url = "https://twitter.com/intent/tweet?hashtags=chucknorris,joke&related=freecodecamp&url=" + getCurrentUrl() + "&text=" + encodeURIComponent(quote);
+    $("#tweet-quote").attr("href", url);
+  }
+}
+
+function getCurrentUrl(){
+    var url = window.location.href;
+      
+      if (url.indexOf("local") > -1)
+      {
+          url = "";
+      }
+      
+      return url;
+}
 
 $(document).ready(function () {
+
+    $.fn.extend({
+        animateCss: function (animationName) {
+            var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+            $(this).addClass('animated ' + animationName).one(animationEnd, function() {
+                $(this).removeClass('animated ' + animationName);
+            });
+        }
+    });
 
     $.getJSON(apiKey, parseQuoteData);
 
